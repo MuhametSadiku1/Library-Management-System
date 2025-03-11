70d634134e9d240d5a0a62f919b1b8620de165ba
Muhamet <muhametsadiku655@gmail.com>
2025-03-11 21:50:20 +0100

    implement qstash workflows for user onboarding

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -1,12 +1,46 @@
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 import { serve } from "@upstash/workflow/nextjs";
+import { eq } from "drizzle-orm";
+
+type UserState = "non-active" | "active";
 
 type InitialData = {
   email: string;
+  fullName: string;
+};
+
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const THREE_DAYS_IN_MS = 3 * ONE_DAY_IN_MS;
+const THIRTY_DAYS_IN_MS = 30 * ONE_DAY_IN_MS;
+
+const getUserState = async (email: string): Promise<UserState> => {
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+  if (user.length === 0) return "non-active";
+
+  const lastActivityDate = new Date(user[0].lastActivityDate!);
+  const now = new Date();
+  const timeDifference = now.getTime() - lastActivityDate.getTime();
+
+  if (
+    timeDifference > THREE_DAYS_IN_MS &&
+    timeDifference <= THIRTY_DAYS_IN_MS
+  ) {
+    return "non-active";
+  }
+
+  return "active";
 };
 
 export const { POST } = serve<InitialData>(async (context) => {
-  const { email } = context.requestPayload;
+  const { email, fullName } = context.requestPayload;
 
+  // Welcome Email
   await context.run("new-signup", async () => {
     await sendEmail("Welcome to the platform", email);
   });
@@ -15,7 +49,7 @@ export const { POST } = serve<InitialData>(async (context) => {
 
   while (true) {
     const state = await context.run("check-user-state", async () => {
-      return await getUserState();
+      return await getUserState(email);
     });
 
     if (state === "non-active") {
@@ -36,10 +70,3 @@ async function sendEmail(message: string, email: string) {
   // Implement email sending logic here
   console.log(`Sending ${message} email to ${email}`);
 }
-
-type UserState = "non-active" | "active";
-
-const getUserState = async (): Promise<UserState> => {
-  // Implement user state logic here
-  return "non-active";
-};
